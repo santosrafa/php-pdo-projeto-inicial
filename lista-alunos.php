b75136774144b132451b146142722d683242349e
Your Name <ti.rafaelteles@gmail.com>
2022-11-30 22:15:44 -0300

    FETCH_ASSOC

--- lista-alunos.php
+++ lista-alunos.php
@@ -1,12 +1,28 @@
 <?php
 
 /* --Criando uma conexao com o banco de dados-- */
+
+use Alura\Pdo\Domain\Model\Student;
+
+require_once 'vendor/autoload.php';
+
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
 /* --fetchAll() retornara um array da consulta executada abaixo-- */
 $statement = $pdo->query('SELECT * FROM students;');
-$studentList = $statement->fetchAll();
 
-echo $studentList[0]['name'];
+/* --PDO::FETCH_ASSOC: Retorna cada linha como um array associativo, onde a chave é o nome da coluna, e o valor é o valor da coluna em si-- */
+$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$studentList = [];
+
+foreach ($studentDataList as $studentData){
+    $studentList[] = new Student(
+        $studentData['id'],
+        $studentData['name'],
+        new \DateTimeImmutable($studentData['birth_date'])
+    );
+}
+
+var_dump($studentList);
 

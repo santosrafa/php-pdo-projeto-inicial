c16ca04fe0deea6b4333fa13c22284d56d4f066b
Your Name <ti.rafaelteles@gmail.com>
2022-12-02 07:09:34 -0300

    prepare_statement

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -9,6 +9,11 @@ $pdo = new PDO('sqlite:' . $databasePath);
 
 $student = new Student(null, 'Vinicius Dias', new \DateTimeImmutable('1997-10-15'));
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(1,$student->name());
+$statement->bindValue(2,$student->birthDate()->format('Y-m-d'));
 
-var_dump($pdo->exec($sqlInsert));
+if ($statement->execute()){
+    echo "Aluno inserido";
+}

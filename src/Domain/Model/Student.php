99be3317d837a268aaa2466fc188d55b01d834bb
TI GIIRO <ti.rafaelteles@gmail.com>
2022-12-15 17:06:28 -0300

    Phone.php

--- src/Domain/Model/Student.php
+++ src/Domain/Model/Student.php
@@ -7,6 +7,7 @@ class Student
     private ?int $id;
     private string $name;
     private \DateTimeInterface $birthDate;
+    private array $phones = [];
 
     public function __construct(?int $id, string $name, \DateTimeInterface $birthDate)
     {
@@ -49,5 +50,15 @@ class Student
         return $this->birthDate
             ->diff(new \DateTimeImmutable())
             ->y;
+    }
+
+    public function addPhone (Phone $phone): void
+    {
+        $this->phones[] = $phone;
+    }
+
+    public function phones(): array
+    {
+        return $this->phones;
     }
 }
